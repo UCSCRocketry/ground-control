658f7b1a5ac272b8fe0ebce3586cc7f61ac201e6
Michael <ciaowwu@gmail.com>
2024-04-10 22:17:01 -0700

    updated comments & added multi plot option

--- test_rt_plotting.py
+++ test_rt_plotting.py
@@ -9,13 +9,16 @@ import matplotlib.pyplot as plt
 #       - list_intervals = [10, 20, 30, 50, 75, 100]        (default)
 #       - iterations = 100
 #       - trials = 1
+# Also, you can try using the multi_rt_plotter (see commented line in for loop below).
+# The multi_rt_plotter plots multiple real time graphs at once.
+# multi_rt_plotter(num_plots, rows, cols, interval, max_iterations)
 
 # I will try to add comments to both this file and real_time_plotting.py in the near future. -Michael Wu
 
 # --- SETTINGS ---
 
 list_intervals = [10, 20, 30, 50, 75, 100]          # a list of animate intervals to test, in milliseconds (ms)
-iterations = 100                                    # num of times animate is called per trial
+max_iterations = 100                                    # num of times animate is called per trial
 trials = 1                                          # num of trials per interval
 
 # --- END OF SETTINGS ---
@@ -28,11 +31,13 @@ test_start = dt.datetime.now()
 for interval in list_intervals:
     trial_avg = 0
     for i in range(trials):
-        rt = rt_plt.rt_plotter(interval, iterations)
+        rt = rt_plt.rt_plotter(interval, max_iterations)
+        #rt = rt_plt.multi_rt_plotter(4, 2, 2, interval, iterations)        # multi plotter option, generates 4 plots in a 2x2 grid
+
         trial_start = dt.datetime.now()
         rt.start()
         trial_end = dt.datetime.now()
-        trial_avg += ((trial_end - trial_start).total_seconds() * 1000) / iterations
+        trial_avg += ((trial_end - trial_start).total_seconds() * 1000) / max_iterations
     trial_avg = round(trial_avg / trials, 5)
 
     print('INTERVAL: {0:<4} ms      AVERAGE: {1:<7} ms'.format(interval, trial_avg))
