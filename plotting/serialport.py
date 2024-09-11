703231ec197fe49bd54d0b2de222e0a4237b704a
Michael Wu <ciaowwu@gmail.com>
2024-09-11 01:14:51 -0700

    refactor: moved MockSerialport class here

--- plotting/serialport.py
+++ plotting/serialport.py
@@ -37,3 +37,40 @@ class Serialport:
     def close(self):
         print(f'{self.list()}\nClosing port {self.ser.name}...')
         self.ser.close()
+
+# mock serial port
+# can write and read bytes to itself
+# init: opens the mock port, sets read timeout to read_timeout
+# write: writes an integer
+# write_mult: writes num bytes one after the other
+# read: reads num_bytes (default 1), blocking for read_timeout seconds
+# close: closes port
+class MockSerialport:
+
+    def __init__(self, read_timeout= 0):
+        self.ser = serial.serial_for_url('loop://', timeout=read_timeout)
+        print('Initialized mock serial port...')
+
+    def write(self, bytes_to_send=None):
+        if bytes_to_send == None:
+            bytes_to_send = randint(-10, 10)
+        print(f'{self.ser.name} Writing: {bytes_to_send}')
+        self.ser.write(int.to_bytes(bytes_to_send, signed=True))
+
+    def write_mult(self, num= 1000, min= -10, max= 10):
+        random_num = int
+        print(f'Writing {num} bytes to port...')
+        for i in range(num):
+            random_num = randint(min, max)
+            self.ser.write(int.to_bytes(random_num, signed=True))
+
+    def read(self, num_bytes=1):
+        if not self.ser.in_waiting:
+            return None
+        received = int.from_bytes(self.ser.read(num_bytes), signed=True)
+        print(f'Received: {received}')
+        return received
+
+    def close(self):
+        print(f'Remaining bytes in port: {self.ser.in_waiting}\nClosing port...')
+        self.ser.close()
