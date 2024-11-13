672f3561a3e8cd87c547e6b19f936470af558441
Sage Silberman <saasilbe@ucsc.edu>
2024-11-13 13:12:32 -0800

    fix merge conflicts

--- gcs-react-app/src/components/NavBar.jsx
+++ gcs-react-app/src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import BackButton from "./../components/BackButton";
+import HomeButton from "./../components/HomeButton";
+
 export default function NavBar() {
     return (
         <h1 id="nav">
             <img src="/GroundControlLogo.png" alt="Logo" />
+            <HomeButton />
+            <BackButton />
         </h1>
     )
 }
