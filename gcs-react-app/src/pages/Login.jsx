ffb983efc85e1310240837f6ce798aee0e79859e
Sarone Kelete <skelete@ucsc.edu>
2024-11-13 13:05:42 -0800

    background and username password containers completed, need to fix spacing and sizing

--- gcs-react-app/src/pages/Login.jsx
+++ gcs-react-app/src/pages/Login.jsx
@@ -1,15 +1,21 @@
 import BackButton from "./../components/BackButton";
 import HomeButton from "./../components/HomeButton";
 import RocketLogo from "./../assets/RocketEmblem.png";
+import "../styles/Login.css";
 
 export default function Home() {
     return (
         <div>
-            <img src={RocketLogo} />
-            <h1>Login</h1>
-            <p>Welcome to the login page!</p>
-            <HomeButton />
-            <BackButton />
+            <div className="center">
+                <img src={RocketLogo} width="50%"/>
+                <input type="text" id="username" name="username"/>
+                <input type="password" id="password" name="password"/>
+                <h6>Forgot Password?</h6>
+            </div>
         </div>
     )
 }
+
+//<p>Welcome to the login page!</p>
+//<HomeButton />
+//<BackButton />
