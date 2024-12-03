b7894a7fe4a81a73819c4718c2fb48d048635a37
Michael <ciaowwu@gmail.com>
2024-12-02 16:37:42 -0800

    feat: add real time websocket updates

--- gcs-react-app/backend/base.py
+++ gcs-react-app/backend/base.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
@@ -7,7 +10,7 @@ from random import randint
 api = Flask(__name__)
 api.config['SECRET_KEY'] = 'secret!'
 CORS(api, resources={r"/*": {"origins": "http://localhost:3000"}})     # allows requests to /api/ pages from origin localhost:3000 (frontend)
-socketio = SocketIO(api, cors_allowed_origins="*")
+socketio = SocketIO(api, async_mode="eventlet", cors_allowed_origins="*", ping_interval=5, always_connect=True)
 
 thread = None
 thread_lock = Lock()
@@ -19,16 +22,17 @@ def background_thread():
     global velocity, accel
     count = 0
     while True:
-        socketio.sleep(10)
+        socketio.sleep(5)
         count += 1
         velocity += randint(-3, 10)
         accel += randint(-3, 10)
         print('Sending data...')
-        socketio.emit('send_data',
+        emit('send_data',
                       {'data': 'Server generated event', 
                        'velocity': velocity,
                        'accel': accel,
-                       'count': count})
+                       'count': count},
+                       broadcast=True)
 
 @socketio.on("connect")
 def connect_msg():
@@ -38,13 +42,20 @@ def connect_msg():
     global thread
     with thread_lock:
         if thread is None:
+            print("Starting background thread...")
             thread = socketio.start_background_task(background_thread())
     emit('connected', {'data': f"id: {request.sid} is connected."})
     
 
 @socketio.on("disconnect")
 def disconnect_msg():
+    global thread
+    thread = None
     print('Client disconnected!')
+
+@socketio.on("reconnect")
+def reconnect_msg():
+    print('Client reconnected!')
 
 @api.route('/api/test')
 def testroute():
