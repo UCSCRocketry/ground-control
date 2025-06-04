4c8e69bbea60279f6da004fe89e2bd2278934fa9
Michael <ciaowwu@gmail.com>
2025-06-03 20:40:03 -0700

    test: update test data generator

--- gcs-react-app/backend/generate_data.py
+++ gcs-react-app/backend/generate_data.py
@@ -1,4 +1,5 @@
 from random import randint
+from serial2num_PORT import PAYLOAD, crc_encode
 
 '''
 SENSOR IDs:
@@ -21,12 +22,29 @@ def generate(ser):
     for i in range(n):
         rsensor = randint(0, 3)
         timestamp += randint(50, 500)
-        b = randint(1, 3)
-        data = bytes([0x21])
-        data += SENSORS[rsensor].to_bytes(2, 'big')
+        b = PAYLOAD.get(SENSORS[rsensor])
+        #data = bytes([0x21])
+        data = SENSORS[rsensor].to_bytes(2, 'big')
         data += timestamp.to_bytes(4, 'big')
-        data += b.to_bytes(1, 'big')
         for j in range(b):
             data += int(randint(0, 512)).to_bytes(2, "big")
-        data += (0x2A2B0D0A).to_bytes(4, 'big')
+        data += crc_encode(data)
+        data = 0x21.to_bytes(1, 'big') + data
+        data += (0x0D0A).to_bytes(2, 'big')
+        print(data)
         ser.write(data)
+
+def generateTest(ser):
+    timestampTEST = 1002321
+    data = bytes([0x21])
+    data += SENSORS[0].to_bytes(2, 'big')
+    data += timestampTEST.to_bytes(4, 'big')
+    b = PAYLOAD.get(SENSORS[0])
+    for j in range(b):
+        data += int(417).to_bytes(2, 'big')
+    data += (0x2A2B0D0A).to_bytes(4, 'big')
+    print(data)
+    for item in data:
+        print(item, end=" ")
+    print("")
+    ser.write(data)
