57651b61ee3b5d3d63fb7c848a0bcb993cc4bed9
Michael Wu <michael.cj.wu@gmail.com>
2025-10-28 23:14:31 -0700

    feat: update random packet generator to new UART standard

--- gcs-react-app/backend/generate_data.py
+++ gcs-react-app/backend/generate_data.py
@@ -1,50 +1,48 @@
-from random import randint
-from serial2num_PORT import PAYLOAD, crc_encode
+import random
+from serial2num_PORT import crc_encode
 
 '''
 SENSOR IDs:
 
-BAROMETER:      ba  0x6261
-GPS:            gp  0x6770
-ACCELOMETER:    ac  0x6163
-GYROSCOPE:      ro  0x726F
+BAROMETER:              ba  0x6261
+HIGHG ACCELEROMETER:    ah  0x6168
+LOWG ACCELEROMETER:     al  0x616C
+GYROSCOPE:              ro  0x726F
 
 '''
 
-SENSORS = [0x6261, 0x6770, 0x6163, 0x726F]
+SENSORS = [0x6261, 0x6168, 0x616C, 0x726F]
 
 timestamp = 0
+seqid = 0
 
 def generate(ser):
-    global timestamp
-    n = randint(0, 4)
-    timestamp += randint(1000, 10000)
+    global timestamp, seqid
+    n = random.choices([0,1,2,3,4,5], weights=[10, 20, 40, 50, 50, 40])[0]
+    timestamp += random.randint(1000, 10000)
     for i in range(n):
-        rsensor = randint(0, 3)
-        timestamp += randint(50, 500)
-        b = PAYLOAD.get(SENSORS[rsensor])
-        #data = bytes([0x21])
-        data = SENSORS[rsensor].to_bytes(2, 'big')
-        data += timestamp.to_bytes(4, 'big')
-        for j in range(b):
-            data += int(randint(0, 512)).to_bytes(2, "big")
-        data += crc_encode(data)
-        data = 0x21.to_bytes(1, 'big') + data
-        data += (0x0D0A).to_bytes(2, 'big')
-        print(data)
-        ser.write(data)
+        seqid += (random.choices([1,2,3], weights=[90, 10, 5])[0])
+        rsensor = random.randint(0, 3)
+        sensorid = SENSORS[rsensor].to_bytes(length=2)
+        timestamp += random.randint(50, 500)
+        if rsensor == 3:    # ro
+            payload = 0x000058.to_bytes(length=3)
+            rX = random.randint(25, 1000).to_bytes(length=4)
+            rY = random.randint(25, 1000).to_bytes(length=4)
+            rZ = random.randint(100, 10000).to_bytes(length=4)
+            payload += rX + 0x59.to_bytes(length=1) + rY + 0x60.to_bytes(length=1) + rZ
+        elif rsensor == 0:  # ba
+            payload = random.randint(111, 999).to_bytes(length=17)
+        else:   # ah, al
+            payload = random.randint(100, 100000).to_bytes(length=17)
+        
+        packet = bytes()
+        packet += seqid.to_bytes(length=4) + sensorid + timestamp.to_bytes(length=4) + payload
+        crc = crc_encode(packet)
 
-def generateTest(ser):
-    timestampTEST = 1002321
-    data = bytes([0x21])
-    data += SENSORS[0].to_bytes(2, 'big')
-    data += timestampTEST.to_bytes(4, 'big')
-    b = PAYLOAD.get(SENSORS[0])
-    for j in range(b):
-        data += int(417).to_bytes(2, 'big')
-    data += (0x2A2B0D0A).to_bytes(4, 'big')
-    print(data)
-    for item in data:
-        print(item, end=" ")
-    print("")
-    ser.write(data)
+        start = 0x21.to_bytes(length=1)
+        end = 0x0D0A.to_bytes(length=2)
+
+        packet = start + packet + crc + end
+        ser.write(packet)
+        # print(f'Writing: seqid: {seqid}, id: {sensorid}, timestamp: {timestamp}, payload: {payload}')
